7285ce93341f9b33e30a3ba0f057fab8163b320e
Alwolfdeadman <alwolf.deadman@gmail.com>
2023-09-25 15:04:51 +0300

    created the spider and added pipelines and items

--- Assignment/Assignment/pipelines.py
+++ Assignment/Assignment/pipelines.py
@@ -1,13 +1,24 @@
 # Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
 
 class AssignmentPipeline:
     def process_item(self, item, spider):
+        price = 'price'
+        addapter = ItemAdapter(item)
+
+        f_names = addapter.field_names()
+        for f_name in f_names:
+            val = addapter.get(f_name)
+            addapter[f_name] = val[0].strip()
+
+        val = addapter.get(price)
+        val = val.replace('£', '')
+        addapter[price] = float(val)
+
+        str_ = addapter.get('color')
+        split_str = str_.split(' ')
+        addapter['color'] = str(split_str[0])
+
         return item
--- Assignment/Assignment/spiders/spider_first.py
+++ Assignment/Assignment/spiders/spider_first.py
@@ -1,10 +1,23 @@
 import scrapy
+from Assignment.Assignment.items import ClothingItem
 
 
 class SpiderFirstSpider(scrapy.Spider):
     name = "spider_first"
     allowed_domains = ["shop.mango.com"]
-    start_urls = ["https://shop.mango.com"]
+    start_urls = ["https://shop.mango.com/gb/women/skirts-midi/midi-satin-skirt_17042020.html?c=99", "https://shop.mango.com/bg-en/men/t-shirts-plain/100-linen-slim-fit-t-shirt_47095923.html?c=07"]
 
     def parse(self, response):
-        pass
+        sizes = []
+        clothing = ClothingItem()
+
+        for size in response.css('ul.tGykN'):
+            sizes.append(size.css('li button snap.text-title-m::text').gett())
+
+        clothing['name'] = response.css('.product-name::text').get()
+        clothing['price'] = response.css('snap.text-title-x1::text').get()
+        clothing['color'] = response.css('div.color-container--selected').attrib['aria-label']
+        clothing['size'] = sizes
+
+        yield clothing
+
